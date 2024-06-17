0fdf8a10316c5b4942e42b0818780e72fdab44a6
Nishan123 <girinishan1234567890@gmail.com>
2024-06-17 13:52:01 +0545

    login page UI

--- src/bhansaghar/BhansaGhar.java
+++ src/bhansaghar/BhansaGhar.java
@@ -4,6 +4,8 @@
  */
 package bhansaghar;
 
+import view.LoginScreen;
+
 /**
  *
  * @author Nishan Giri
@@ -18,6 +20,11 @@ public class BhansaGhar {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        LoginScreen mainFrame = new LoginScreen();
+        mainFrame.setVisible(true);
+        mainFrame.setSize(1200, 1600);
+        mainFrame.setDefaultCloseOperation(LoginScreen.EXIT_ON_CLOSE);
+      
     }
     
 }
