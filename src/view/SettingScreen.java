7404b5f3c8613262aa46c48379fdedc68216c9f3
Nishan123 <girinishan1234567890@gmail.com>
2024-08-17 22:08:10 +0545

    Logout feature added

--- src/view/SettingScreen.java
+++ src/view/SettingScreen.java
@@ -404,6 +404,9 @@ public class SettingScreen extends javax.swing.JFrame {
 
     private void jButton5ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton5ActionPerformed
         // TODO add your handling code here:
+        LoginScreen loginScreen = new LoginScreen();
+        loginScreen.setVisible(true);
+        this.dispose();
     }//GEN-LAST:event_jButton5ActionPerformed
 
     private void jButton9ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton9ActionPerformed
